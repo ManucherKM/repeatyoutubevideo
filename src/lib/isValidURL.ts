7dcb6782f2f169fae3573769bb1bc51cc63a561f
ManucherKM <manucher20056@gmail.com>
2025-01-05 21:51:21 +0500

    .

--- src/lib/isValidURL.ts
+++ src/lib/isValidURL.ts
@@ -1,7 +1,8 @@
 export function isValidHttpUrl(string: string) {
   try {
     new URL(string);
-  } catch (_) {
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+  } catch (_) { /* eslint-disable @typescript-eslint/no-unused-vars */
     return false;
   }
 
